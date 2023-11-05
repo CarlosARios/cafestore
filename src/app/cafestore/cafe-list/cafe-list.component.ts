222f1d7edd63879217f011bd8a7e3ccd55510de9
Carlos Rios <c.riosp@uniandes.edu.co>
2023-11-04 20:12:42 -0500

    Finalizacion de punto DOS

--- src/app/cafestore/cafe-list/cafe-list.component.ts
+++ src/app/cafestore/cafe-list/cafe-list.component.ts
@@ -11,16 +11,26 @@ export class CafeListComponent implements OnInit {
 
 
   cafes: Array<Cafe> = [];
+  blendcount: number = 0;
+  origencount: number = 0;
   constructor(private cafestoreService: CafestoreService) { }
 
   getCafes(): void {
     this.cafestoreService.getCafes().subscribe((cafes) => 
     {this.cafes = cafes});
-    
+  }
+
+  async loadData() {
+    this.cafestoreService.getCafes().subscribe((cafes) => {
+      this.cafes = cafes;
+      this.blendcount = this.cafes.filter(cafe => cafe.tipo.toLowerCase().includes('blend')).length;
+      this.origencount = this.cafes.filter(cafe => cafe.tipo.toLowerCase().includes('café de origen')).length;
+    });
   }
 
   ngOnInit() {
-    this.getCafes();
+    this.loadData();
   }
+  
 
 }
